ae5c73c81227d0e2f6c45426fb031b14a0e45477
yesthisiskendra <yesthisiskendra@gmail.com>
2019-06-11 14:35:22 -0700

    Improved the original create react app landing page by adding a cat

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
+import cat from './crazy_cat_lady.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cat} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kendra (previously Osburn) Ryan
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.caterwaul.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Caterwaul
+        </a>
+        <a
+          className="App-link"
+          href="http://showmecats.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Show me cats
         </a>
       </header>
     </div>
